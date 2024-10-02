a4cd0fed5df22e2a78cdd88c378e7575f69d40d9
vivkeravuori <k.vivien@wustl.edu>
2024-10-02 11:41:42 -0400

    changetransitions

--- js/lightbox.js
+++ js/lightbox.js
@@ -285,7 +285,7 @@
 
     // Show loading state
     this.$overlay.fadeIn(this.options.fadeDuration);
-    $('.lb-loader').fadeIn('slow');
+    $('.lb-loader').fadeIn(800);
     this.$lightbox.find('.lb-image, .lb-nav, .lb-prev, .lb-next, .lb-dataContainer, .lb-numbers, .lb-caption').hide();
     this.$outerContainer.addClass('animating');
 
